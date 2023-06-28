75d9c791d8e7bb8cdbaf0cb336a9c2deb12fb304
Caio <bladellano@gmail.com>
2023-06-28 08:54:39 +0100

    Criando UserController; Jwt-auth e gerando usuários com db:seed

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -9,12 +9,11 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
